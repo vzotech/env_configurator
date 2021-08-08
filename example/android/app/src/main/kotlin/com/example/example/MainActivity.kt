9ed86e2d1d94aa452265b3cd6d946a3ff957c56e
Muhammad Ahmed <mahmed8003@gmail.com>
2021-08-08 23:08:46 +0500

    handling all values as string now, performed testing

--- example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.example
 
+
+import EnvConfig
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+
+
+    override fun onStart() {
+        super.onStart()
+
+        val config = EnvConfig(this)
+        Log.d("facebookAppId", config.facebookAppId)
+        Log.d("fbLoginProtocolScheme", config.fbLoginProtocolScheme)
+        Log.d("googleMapsApiKey", config.googleMapsApiKey)
+    }
 }
--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,6 +7,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let config: EnvConfig = EnvConfig()
+    print("facebookAppId \(config.facebookAppId)")
+    print("fbLoginProtocolScheme \(config.fbLoginProtocolScheme)")
+    print("googleMapsApiKey \(config.googleMapsApiKey)")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
